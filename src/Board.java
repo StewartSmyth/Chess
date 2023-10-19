9a9948c02a52f1bbe7712bbf0501e39bd0b6dcf1
Stewbbb <stewart.smyth3@gmail.com>
2023-10-19 15:52:19 +0100

    Board set up fully

--- src/Board.java
+++ src/Board.java
@@ -2,28 +2,43 @@ public class Board {
     Piece[][] board = new Piece[8][8];
     public Board(){
 
-        for(int i=0; i<8; i++){
-            board[0][i] = new Piece("Q", true, true, true, false, false);
-        }
-        for (int i=1;i<8;i++){
-            for(int j = 0; j<8; j++){
-                board[i][j] = new Piece("_", true);
-            }
-        }
-
-
     }
 
     public void setBoard() {
-
+        boolean white = false;
+        for (int row=7;row>=0;row--){
+            if(row==1){
+                white = true;
+            }
+            if(row==0 || row==7){
+                board[row][0] = new Piece("R", white, row ,0);
+                board[row][7] = new Piece("R", white, row ,0);
+                board[row][1] = new Piece("N", white, row ,0);
+                board[row][6] = new Piece("N", white, row ,0);
+                board[row][2] = new Piece("B", white, row ,0);
+                board[row][5] = new Piece("B", white, row ,0);
+                board[row][3] = new Piece("Q", white, row ,0);
+                board[row][4] = new Piece("K", white, row ,0);
+                continue;
+            }
+            for(int column = 0; column<=7; column++){
+                System.out.println(row);
+                if(row == 1 || row == 6){
+                    board[row][column] = new Piece("P", white, row ,column);
+                }
+                else{
+                    board[row][column] = new Piece("_", true);
+                }
+            }
+        }
     }
 
     public void displayBoard(){
-        for(int i = 0; i<8; i++){
+        for(int i = 7; i>=0; i--){
             for(int j = 0; j<8; j++){
-                System.out.print(board[i][j].pieceLetter);
+                System.out.print(board[i][j].letterColour + board[i][j].pieceLetter + " ");
             }
-            System.out.println(" ");
+            System.out.println();
         }
     }
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,24 +1,56 @@
 public class Piece {
     String pieceLetter;
+    String letterColour;
     boolean isWhite;
     protected boolean canMoveStraight = false;
     protected boolean canMoveDiagonal = false;
     protected boolean canMoveHorse = false;
-    protected boolean onlyMoveOneSquare;
+    protected boolean onlyMoveOneSquare = false;
     boolean isEmpty;
+    int xPos;
+    int yPos;
 
 
-    public Piece(String pieceLetter, boolean isWhite, boolean canMoveStraight,boolean canMoveDiagonal,boolean canMoveHorse,boolean onlyMoveOneSquare){
+    public Piece(String pieceLetter, boolean isWhite, int xPos, int yPos){
         this.pieceLetter = pieceLetter;
         this.isWhite = isWhite;
-        this.canMoveStraight = canMoveStraight;
-        this.canMoveDiagonal = canMoveDiagonal;
-        this.canMoveHorse = canMoveHorse;
-        this.onlyMoveOneSquare = onlyMoveOneSquare;
+        if(isWhite){
+            letterColour = "W";
+        }
+        else{
+            letterColour = "B";
+        }
+        switch(pieceLetter){
+            case "Q":
+                canMoveStraight = true;
+                canMoveDiagonal = true;
+                break;
+            case "K":
+                onlyMoveOneSquare = true;
+                canMoveDiagonal = true;
+                canMoveStraight = true;
+                break;
+            case "P":
+                onlyMoveOneSquare = true;
+                canMoveStraight = true;
+                break;
+            case "R":
+                canMoveStraight = true;
+                break;
+            case "B":
+                canMoveDiagonal = true;
+            case "N":
+                canMoveHorse = true;
+                break;
+        }
+        this.xPos = xPos;
+        this.yPos = yPos;
     }
+
     public Piece(String pieceLetter, boolean isEmpty){
         this.pieceLetter = pieceLetter;
         this.isEmpty = isEmpty;
+        letterColour = "_";
     }
 
     public void Move(int moveX, int moveY){
